20d17914052b1ee473c81bb934765b6e68a7df35
Alexander Chmykhov <alexander.chmykhov@gmail.com>
2023-10-26 16:32:38 +0300

    Lesson4 part 6 bones finished

--- Lesson4/app/src/main/java/ru/mirea/chmykhovam/lesson4/MainActivity.kt
+++ Lesson4/app/src/main/java/ru/mirea/chmykhovam/lesson4/MainActivity.kt
@@ -7,7 +7,6 @@ import ru.mirea.chmykhovam.lesson4.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
--- Lesson4/serviceapp/src/main/java/ru/mirea/chmykhovam/serviceapp/MainActivity.kt
+++ Lesson4/serviceapp/src/main/java/ru/mirea/chmykhovam/serviceapp/MainActivity.kt
@@ -1,11 +1,60 @@
 package ru.mirea.chmykhovam.serviceapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest.permission.FOREGROUND_SERVICE
+import android.Manifest.permission.POST_NOTIFICATIONS
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import ru.mirea.chmykhovam.serviceapp.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    private var buttonState = 0
+    private val permissionCode = 200
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        if (ContextCompat.checkSelfPermission(
+                this, POST_NOTIFICATIONS
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+
+            Log.d(MainActivity::class.java.simpleName, "Разрешения получены")
+        } else {
+            Log.d(MainActivity::class.java.simpleName, "Нет разрешений!")
+
+            ActivityCompat.requestPermissions(
+                this, arrayOf(POST_NOTIFICATIONS, FOREGROUND_SERVICE), permissionCode
+            )
+
+        }
+    }
+
+    fun onButtonClick(v: View) {
+        if (buttonState == 0) {
+            val serviceIntent = Intent(this@MainActivity, PlayerService::class.java)
+            ContextCompat.startForegroundService(this@MainActivity, serviceIntent)
+            binding.buttonPlay.setImageResource(R.drawable.baseline_stop)
+            binding.imageAlbumArt.setImageResource(R.drawable.joe_hisaishi_merry_go_round_of_life)
+            binding.textTitle.text = getString(R.string.merry_go_round)
+            binding.textArtist.text = getString(R.string.howl_s_walking_castle)
+            buttonState = 1
+        }else if (buttonState == 1){
+            stopService(Intent(this@MainActivity, PlayerService::class.java))
+            binding.buttonPlay.setImageResource(R.drawable.baseline_play)
+            binding.imageAlbumArt.setImageResource(R.mipmap.ic_launcher)
+            binding.textTitle.text = getString(R.string.current_song)
+            binding.textArtist.text = getString(R.string.artist_name)
+            buttonState = 0
+        }else{
+            throw IllegalStateException("Unknown state code")
+        }
     }
 }
